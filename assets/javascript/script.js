316dfed6dba0b8980d8c6cb19c6bcdcc01b3f276
ski14hs <sean.k.ishikawa@gmail.com>
2018-09-20 19:08:50 -0700

    first js changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,23 @@
 
 
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAdaj-eidUQPCcKYrVK24WpG3IC224ShY8",
-    authDomain: "timeclockapp-affa7.firebaseapp.com",
-    databaseURL: "https://timeclockapp-affa7.firebaseio.com",
-    projectId: "timeclockapp-affa7",
-    storageBucket: "",
-    messagingSenderId: "766938401677"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyAdaj-eidUQPCcKYrVK24WpG3IC224ShY8",
+authDomain: "timeclockapp-affa7.firebaseapp.com",
+databaseURL: "https://timeclockapp-affa7.firebaseio.com",
+projectId: "timeclockapp-affa7",
+storageBucket: "",
+messagingSenderId: "766938401677"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 function addEmployee(){
     //check for validation
+    var name = $("#ee-name").val().trim();
+    var role = $("#ee-role").val().trim();
 
     //push to firebase
     
