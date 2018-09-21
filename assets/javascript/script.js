d4171314e25300799eebf74444c54dd525bb5cda
loeljee <loeljee@gmail.com>
2018-09-20 19:02:05 -0700

    more

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAdaj-eidUQPCcKYrVK24WpG3IC224ShY8",
+    authDomain: "timeclockapp-affa7.firebaseapp.com",
+    databaseURL: "https://timeclockapp-affa7.firebaseio.com",
+    projectId: "timeclockapp-affa7",
+    storageBucket: "",
+    messagingSenderId: "766938401677"
+  };
+  firebase.initializeApp(config);
